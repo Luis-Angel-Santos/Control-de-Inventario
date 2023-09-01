6d9f9b60fabba62bd0e093de545e345cebb3ef80
Luis Angel Santos <santosangel748@gmail.com>
2023-09-01 12:49:37 -0600

    Detalles finales del menu

--- src/app/modules/shared/components/sidenav/sidenav.component.ts
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -10,6 +10,11 @@ export class SidenavComponent {
 
   media = inject(MediaMatcher);
   mobileQuery: MediaQueryList;
+  menuNav = [
+    {name: 'Home', route: 'home', icon: 'home'},
+    {name: 'Categorias', route: 'category', icon: 'category'},
+    {name: 'Productos', route: 'product', icon: 'production_quantity_limits'},
+  ];
 
   constructor(){
     this.mobileQuery = this.media.matchMedia('(max-width: 600px)');
